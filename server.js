08ab7c068a9fab902eca098e620b3a3eefe62069
Sudheer Kumar <always.sudheer@gmail.com>
2021-03-14 15:34:25 +0530

    Update mongoose url, load model, register routes and install bodyParser

--- server.js
+++ server.js
@@ -2,6 +2,21 @@ var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000;
 
-app.listen(port);
+mongoose = require('mongoose'),
+  Task = require('./api/models/ProductListModel'), //created model loading here
+  bodyParser = require('body-parser');
+  
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/shoppingCartDB'); 
 
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var routes = require('./api/routes/ProductListRoutes'); //importing route
+routes(app); //register the route
+
+
+app.listen(port);
 console.log('Product list RESTful API server started on: ' + port);
